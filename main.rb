743c49b8e15be6a97eaecb421f07cdb3d20d5130
Travis Riggs <travis.a.riggs@gmail.com>
2014-05-05 17:14:19 -0400

    * Created initial views for game * Modified layout.erb to include a 'Start Over' link

--- main.rb
+++ main.rb
@@ -4,5 +4,24 @@ require 'sinatra'
 set :sessions, true
 
 
+get '/' do
+  redirect '/welcome'
+end
+
+get '/welcome' do
+  erb :welcome
+end
+
+post '/bet' do
+  erb :bet
+end
+
+post '/game' do
+  erb :game
+end
+
+# get '/game' do
+#   erb :game
+# end
 
 
