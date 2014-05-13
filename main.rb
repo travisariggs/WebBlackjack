bc268a2ea0fd08469ef4755cf21be422f6ed4277
Travis Riggs <travis.a.riggs@gmail.com>
2014-05-12 22:26:05 -0400

    * Changes to implement a working game of blackjack. Still need to hide the dealer's hand and implement the card images

--- main.rb
+++ main.rb
@@ -3,13 +3,64 @@ require 'sinatra'
 
 set :sessions, true
 
+helpers do
+
+  def calculate_hand(hand)
+    # hand = An array of cards, which are arrays
+    #  of [value, suit]. The value is a string
+    #  of '1', '2', ..., 'J', 'Q', 'K', 'A'.
+    #  The suit is 'C', 'D', 'H' or 'S'
+    total = 0
+    values = hand.map { |c| c[0] }
+    values.each do |value|
+
+      total += case value
+        when 'A' then 11
+        when 'K' then 10
+        when 'Q' then 10
+        when 'J' then 10
+        else value.to_i
+      end
+
+    end
+
+    # If it's a bust, can we convert any aces to 1's?
+    if total > 21
+      values.select{ |c| c == 'A' }.length.times do 
+        total -= 10
+        break if total <= 21
+      end
+    end
+
+    total
+
+  end
+
+  def hand_status?(hand_total)
+
+    if hand_total == 21
+      status = 'Blackjack'
+    elsif hand_total > 21
+      status = 'Bust'
+    else
+      status = 'Playing'
+    end
+
+  end
+
+  def show_card(card)
+
+  end
+
+end
+
+before do
+  @show_hit_stay_buttons = true
+  @turn = 'Player'
+end
 
 get '/' do
-  if session[:username]
-    redirect '/bet'
-  else
-    redirect '/welcome'
-  end
+  redirect '/welcome'
 end
 
 get '/welcome' do
@@ -18,7 +69,8 @@ end
 
 post '/welcome' do
   session[:username] = params[:username]
-  session[:money] = params[:money]
+  session[:money] = params[:money].to_i
+  session[:starting_money] = params[:money].to_i
   # Progress to the game
   redirect '/bet'
 end
@@ -32,7 +84,7 @@ get '/bet' do
 end
 
 post '/bet' do
-  session[:wager] = params[:wager]
+  session[:wager] = params[:wager].to_i
   redirect '/game'
 end
 
@@ -44,26 +96,215 @@ get '/game' do
   end
 
   # Have we already started playing?
-  if session[:shoe] && session[:dealer_hand] && session[:player_hand]
-    erb :game
-    
-  else # Setup a new round
-    # Setup Shoe of Cards
-    suits = ['C', 'D', 'H', 'S']
-    cards = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-    session[:shoe] = cards.product(suits).shuffle
+  # if session[:shoe] && session[:dealer_hand] && session[:player_hand]
+  #   erb :game
 
-    # Deal Cards
-    session[:dealer_hand] = []
-    session[:player_hand] = []
+  # else 
 
-    session[:dealer_hand] << session[:shoe].pop
-    session[:player_hand] << session[:shoe].pop
-    session[:dealer_hand] << session[:shoe].pop
-    session[:player_hand] << session[:shoe].pop
+  #@@@@ Setup a new round
+  # Setup Shoe of Cards
+  suits = ['C', 'D', 'H', 'S']
+  cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  session[:shoe] = cards.product(suits).shuffle
 
-    erb :game
+  # Deal Cards
+  session[:dealer_hand] = []
+  session[:player_hand] = []
+
+  session[:dealer_hand] << session[:shoe].pop
+  session[:player_hand] << session[:shoe].pop
+  session[:dealer_hand] << session[:shoe].pop
+  session[:player_hand] << session[:shoe].pop
+
+  # Set status variable for 'PlayersTurn'
+  session[:game_status] = 'PlayersTurn'
+
+  # Check initial game status for Blackjack
+  total = calculate_hand(session[:player_hand])
+  status = hand_status?(total)
+
+  # Player blackjack?
+  if calculate_hand(session[:player_hand]) == 21
+
+    @success = "Blackjack, baby!"
+
+    session[:money] += 2*session[:wager]
+
+    @show_hit_stay_buttons = false
+    @show_play_again_quit_buttons = true
+
+  elsif calculate_hand(session[:dealer_hand]) == 21
+
+    @error = "Dealer has Blackjack"
+
+    session[:money] -= session[:wager]
+
+    @show_hit_stay_buttons = false
+    @show_play_again_quit_buttons = true
+
   end
+
+  erb :game 
+  
+  #end
+
+end
+
+post '/game/player/hit' do
+  session[:player_hand] << session[:shoe].pop
+
+  total = calculate_hand(session[:player_hand])
+  status = hand_status?(total)
+
+  if status == 'Blackjack'
+
+    @success = "Blackjack, baby!"
+
+    session[:money] += 2*session[:wager]
+
+    @show_hit_stay_buttons = false
+    @show_play_again_quit_buttons = true
+
+  elsif status == 'Bust'
+
+    @error = "Sorry, #{session[:username]}, you busted"
+
+    session[:money] -= session[:wager]
+
+    @show_hit_stay_buttons = false
+    @show_play_again_quit_buttons = true
+
+  end
+
+  erb :game
+end
+
+post '/game/player/stay' do
+  @show_hit_stay_buttons = false
+  @info = "#{session[:username]}, you have decided to stay."
+  @turn = 'Dealer'
+  redirect '/game/dealer'
+end
+
+post '/game/player/quit' do
+  erb :close
+end
+
+post '/game/player/playagain' do
+  redirect '/bet'
+end
+
+get '/game/dealer' do
+  @show_hit_stay_buttons = false
+  @turn = 'Dealer'
+
+  dealer_total = calculate_hand(session[:dealer_hand])
+  dealer_status = hand_status?(dealer_total)
+
+  if dealer_status == 'Blackjack'
+
+    @error = "Dealer has Blackjack"
+
+    @show_play_again_quit_buttons = true
+
+  elsif dealer_status == 'Bust'
+
+    @success = "Dealer busted!  #{session[:username]}, you win!"
+
+    @show_play_again_quit_buttons = true
+
+  elsif dealer_total < 17
+
+    @show_dealer_hit = true
+
+  else # 17, 18, 19, 20
+
+    # Dealer is done, see who won
+    user_total = calculate_hand(session[:player_hand])
+
+    if user_total > dealer_total
+      @success = "#{session[:username]}, you won!"
+      session[:money] += session[:wager]
+    elsif user_total < dealer_total
+      @error = "Sorry #{session[:username]}, you lost this time."
+      session[:money] -= session[:wager]
+    elsif user_total == dealer_total
+      @info = "It's a push."
+    else
+      @error = "SOMETHING WENT TERRIBLY WRONG!"
+    end
+
+    @show_play_again_quit_buttons = true
+
+    # Make decisions about the next round
+    if session[:money] == 0
+      redirect '/game/player/quit'
+    end
+
+  end
+
+  erb :game
+
+end
+
+post '/game/dealer/hit' do
+
+  @show_hit_stay_buttons = false
+  @turn = 'Dealer'
+
+  session[:dealer_hand] << session[:shoe].pop
+
+  dealer_total = calculate_hand(session[:dealer_hand])
+  dealer_status = hand_status?(dealer_total)
+
+  if dealer_status == 'Blackjack'
+
+    @error = "Dealer has Blackjack"
+
+    session[:money] -= session[:wager]
+
+    @show_play_again_quit_buttons = true
+
+  elsif dealer_status == 'Bust'
+
+    @success = "Dealer busted!  #{session[:username]}, you win!"
+
+    session[:money] += session[:wager]
+
+    @show_play_again_quit_buttons = true
+
+  elsif dealer_total < 17
+
+    @show_dealer_hit = true
+
+  elsif [17, 18, 19, 20].include?(dealer_total)
+
+    # Dealer is done, see who won
+    user_total = calculate_hand(session[:player_hand])
+
+    if user_total > dealer_total
+      @success = "#{session[:username]}, you won!"
+      session[:money] += session[:wager]
+    elsif user_total < dealer_total
+      @error = "Sorry #{session[:username]}, you lost this time."
+      session[:money] -= session[:wager]
+    elsif user_total == dealer_total
+      @info = "It's a push."
+    else
+      @error = "SOMETHING WENT TERRIBLY WRONG!"
+    end
+
+    @show_play_again_quit_buttons = true
+
+    # Make decisions about the next round
+    if session[:money] == 0
+      redirect '/game/player/quit'
+    end
+
+  end
+
+  erb :game
+
 end
 
 # post '/game' do
